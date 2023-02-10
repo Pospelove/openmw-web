c0e7c91fc4f24eaba1d1c1e1577fab96df56c512
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-02-11 00:25:35 +0600

    draw empty opengl canvas

--- get.sh
+++ get.sh
@@ -2,3 +2,8 @@ mkdir -p build
 docker run --rm openmwweb cat ./build/boost_hello_world.js > build/boost_hello_world.js
 docker run --rm openmwweb cat ./build/boost_hello_world.wasm > build/boost_hello_world.wasm
 docker run --rm openmwweb cat ./build/boost_hello_world.html > build/boost_hello_world.html
+
+# opengl_hello_world
+docker run --rm openmwweb cat ./build/opengl_hello_world.js > build/opengl_hello_world.js
+docker run --rm openmwweb cat ./build/opengl_hello_world.wasm > build/opengl_hello_world.wasm
+docker run --rm openmwweb cat ./build/opengl_hello_world.html > build/opengl_hello_world.html
